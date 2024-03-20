69ef00dc01c7f8d165a51173596a1e40665c9385
JapneetSinghh <japneet8208@gmail.com>
2024-03-21 04:37:00 +0530

    Connected to mongodb, added backend for login signup and authentication validation

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const router = express.Router();
-router.get('/login',(req,res,next)=>{
-    console.log('Welcome to login page');
-    res.render('authentication/login',{
-        pageTitle:"Login | Taste On Wheels"
-    })
-})
 
-router.get('/signup',(req,res,next)=>{
-    console.log('Welcome to sign up page');
-    res.render('authentication/signup',{
-        pageTitle:"Sign Up | Taste On Wheels"
+const { body } = require('express-validator');
+
+const authController = require('../Controller/auth');
+
+router.get('/login', authController.getLogin);
+router.post('/login', [
+    body('email').isEmail().withMessage('Please Enter A Valid Email'),
+    body('password', 'Password must be of atleast 6 characters').isLength({ min: 6 }).isAlphanumeric()
+],
+    authController.postLogin);
+
+router.get('/signup', authController.getSignUp);
+
+router.post('/signup', [
+    body('firstName').custom((value) => {
+        if (value === '') {
+            throw new Error('Please enter your first name')
+        }
+        return true;
+    }),
+    body('lastName').custom((value) => {
+        if (value === '') {
+            throw new Error('Please enter your last name')
+        }
+        return true;
+    }), body('email').isEmail().withMessage('Please enter a valid email'),
+
+    body('password', 'Password must be of atleast 6 characters').isLength({ min: 6 }).isAlphanumeric(),
+    body('confirmPassword')
+    .custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Passwords Have To Match');
+        }
+        return true;
     })
-})
+], authController.postSignUp);
+
+router.post('/logout', authController.postLogOut);
 
 exports.router = router;
--- Routes/dashboard.js
+++ Routes/dashboard.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/dashboard',(req,res,next)=>{
-    res.render('dashboard/dashboard',{
-        pageTitle:'Dashboard'
-    })
+router.get('/dashboard', (req, res, next) => {
+    console.log('req.session.isLoggedIn:', req.session.isLoggedIn)
+    if (req.session.isLoggedIn) {
+        res.render('dashboard/dashboard', {
+            pageTitle: 'Dashboard'
+        })
+    } else {
+        res.redirect('/login');
+        console.log('Login to use dashboard')
+    }
+})
+router.get('/addFoodTruck', (req, res, next) => {
+    if (req.session.isLoggedIn) {
+        res.render('dashboard/addFoodTruck', {
+            pageTitle: 'Dashboard'
+        })
+    } else {
+        res.redirect('/login');
+        console.log('Login to use dashboard')
+    }
 })
 
 exports.router = router;
