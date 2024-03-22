8276f9350b053d25c16505e1b6d745dd7855ac90
JapneetSinghh <japneet8208@gmail.com>
2024-03-23 05:27:06 +0530

    Update food truck data enabled

--- Controller/dashboard.js
+++ Controller/dashboard.js
@@ -16,6 +16,7 @@ exports.getAddFoodTruck = (req, res, next) => {
     res.render('dashboard/addFoodTruck', {
         pageTitle: 'Dashboard',
         url: '/addFoodTruck',
+        truckId: '',
         message: '',
         className: '',
         name: '',
@@ -127,6 +128,7 @@ exports.postAddFoodTruck = (req, res, next) => {
             className: className,
             pageTitle: 'Dashboard',
             url: '/addFoodTruck',
+            truckId: '',
             name: name,
             address: address,
             priceForTwo: priceForTwo,
@@ -243,7 +245,8 @@ exports.postAddFoodTruck = (req, res, next) => {
                     gallery: [],
                     specialThreeDishes: []
                 }
-            ]
+            ],
+            website:website
         })
 
 
@@ -282,135 +285,189 @@ exports.getMyTrucks = (req, res, next) => {
 }
 exports.getEditMyTruck = (req, res, next) => {
     const truckId = req.params.truckId;
-
-    FoodTruck.findById(truckId)
-    .then(truck=>{
-        res.render('dashboard/addFoodTruck', {
-            pageTitle: 'Dashboard',
-            url: '/addFoodTruck',
-            message: '',
-            className: '',
-            name: truck.name,
-            address: truck.address,
-            priceForTwo: truck.priceForTwo,
-            mondayStatus: truck.openingHours[0].open,
-            tuesdayStatus:truck.openingHours[1].open,
-            wednesdayStatus:truck.openingHours[2].open,
-            thursdayStatus:truck.openingHours[3].open,
-            fridayStatus:truck.openingHours[4].open,
-            saturdayStatus:truck.openingHours[5].open,
-            sundayStatus:truck.openingHours[6].open,
-            openingTimeMonday: truck.openingHours[0].openingTime,
-            closingTimeMonday: truck.openingHours[0].openingTime,
-            openingTimeTuesday: truck.openingHours[1].openingTime,
-            closingTimeTuesday: truck.openingHours[1].openingTime,
-            openingTimeWednesday: truck.openingHours[2].openingTime,
-            closingTimeWednesday: truck.openingHours[2].openingTime,
-            openingTimeThursday: truck.openingHours[3].openingTime,
-            closingTimeThursday: truck.openingHours[3].openingTime,
-            openingTimeFriday: truck.openingHours[4].openingTime,
-            closingTimeFriday: truck.openingHours[4].openingTime,
-            openingTimeSaturday: truck.openingHours[5].openingTime,
-            closingTimeSaturday: truck.openingHours[5].openingTime,
-            openingTimeSunday: truck.openingHours[6].openingTime,
-            closingTimeSunday: truck.openingHours[6].openingTime,
-            cusinesOffered: truck.cusinesOffered,
-            specialDishOne: truck.specialDishOne,
-            specialDishTwo: truck.specialDishTwo,
-            specialDishThree: truck.specialDishThree,
-            famousFor: truck.famousFor,
-            description: truck.description,
-            year: truck.year,
-            discountToday: truck.discountToday,
-            customDiscount1: truck.customDiscount1,
-            customDiscount2: truck.customDiscount2,
-            customDiscount3: truck.customDiscount3,
-            email: truck.email,
-            contactNumbers: truck.contactNumbers,
-            bogoOn: truck.bogoOn,
-            website: truck.website
-        })
-    })
-    .catch(err=>{
-        console.log(err);
-        console.log('Truck not found');
-        return res.redirect('/myTrucks')
-    })
-    
-}
-exports.postEditMyTruck = (req, res, next) => {
-    const truckId = req.body.truckId;
+    console.log(truckId)
 
     FoodTruck.findById(truckId)
         .then(truck => {
-            truck.name = req.body.name;
-            truck.address = req.body.address;
-            truck.priceForTwo = req.body.priceForTwo;
-
-            truck.mondayStatus = req.body.MondayStatus;
-            truck.tuesdayStatus = req.body.TuesdayStatus;
-            truck.wednesdayStatus = req.body.WednesdayStatus;
-            truck.thursdayStatus = req.body.ThursdayStatus;
-            truck.fridayStatus = req.body.FridayStatus;
-            truck.saturdayStatus = req.body.SaturdayStatus;
-            truck.sundayStatus = req.body.SundayStatus;
-
-            truck.openingTimeMonday = req.body.openingTimeMonday;
-            truck.closingTimeMonday = req.body.closingTimeMonday;
-
-            truck.openingTimeTuesday = req.body.openingTimeTuesday;
-            truck.closingTimeTuesday = req.body.closingTimeTuesday;
-
-            truck.openingTimeWednesday = req.body.openingTimeWednesday;
-            truck.closingTimeWednesday = req.body.closingTimeWednesday;
-
-            truck.openingTimeThursday = req.body.openingTimeThursday;
-            truck.closingTimeThursday = req.body.closingTimeThursday;
-
-            truck.openingTimeFriday = req.body.openingTimeFriday;
-            truck.closingTimeFriday = req.body.closingTimeFriday;
-
-            truck.openingTimeSaturday = req.body.openingTimeSaturday;
-            truck.closingTimeSaturday = req.body.closingTimeSaturday;
-
-            truck.openingTimeSunday = req.body.openingTimeSunday;
-            truck.closingTimeSunday = req.body.closingTimeSunday;
-
-            truck.cusinesOffered = req.body.cusinesOffered;
-
-            truck.specialDishOne = req.body.specialDishOne;
-            truck.specialDishTwo = req.body.specialDishTwo;
-            truck.specialDishThree = req.body.specialDishThree;
-
-            truck.famousFor = req.body.famousFor;
-            truck.description = req.body.description;
-            truck.year = req.body.year;
-            truck.discountToday = req.body.discountToday;
-
-            truck.customDiscount1 = req.body.customDiscount1;
-            truck.customDiscount2 = req.body.customDiscount2;
-            truck.customDiscount3 = req.body.customDiscount3;
-
-            truck.email = req.body.email;
-            truck.contactNumbers = req.body.contactNumbers;
-
-            truck.bogoOn = req.body.bogoOn;
-            truck.website = req.body.website;
-
-
-            truck.save()
-                .then(result => {
-                    console.log(result);
-                    return res.redirect('/myTrucks');
-                }).catch(err => {
-                    console.log(err);
-                    console.log('Truck found but could not be updated');
-                    return res.redirect('/myTrucks');
-                })
+            console.log('Truck to be edited', truck);
+            res.render('dashboard/addFoodTruck', {
+                pageTitle: 'Edit Food Truck',
+                url: '/editFoodTruck',
+                truckId: truck._id,
+                message: '',
+                className: '',
+                name: truck.name,
+                address: truck.address,
+                priceForTwo: truck.priceForTwo,
+                mondayStatus: truck.openingHours[0].open,
+                tuesdayStatus: truck.openingHours[1].open,
+                wednesdayStatus: truck.openingHours[2].open,
+                thursdayStatus: truck.openingHours[3].open,
+                fridayStatus: truck.openingHours[4].open,
+                saturdayStatus: truck.openingHours[5].open,
+                sundayStatus: truck.openingHours[6].open,
+                openingTimeMonday: truck.openingHours[0].openingTime,
+                closingTimeMonday: truck.openingHours[0].openingTime,
+                openingTimeTuesday: truck.openingHours[1].openingTime,
+                closingTimeTuesday: truck.openingHours[1].openingTime,
+                openingTimeWednesday: truck.openingHours[2].openingTime,
+                closingTimeWednesday: truck.openingHours[2].openingTime,
+                openingTimeThursday: truck.openingHours[3].openingTime,
+                closingTimeThursday: truck.openingHours[3].openingTime,
+                openingTimeFriday: truck.openingHours[4].openingTime,
+                closingTimeFriday: truck.openingHours[4].openingTime,
+                openingTimeSaturday: truck.openingHours[5].openingTime,
+                closingTimeSaturday: truck.openingHours[5].openingTime,
+                openingTimeSunday: truck.openingHours[6].openingTime,
+                closingTimeSunday: truck.openingHours[6].openingTime,
+                cusinesOffered: truck.cusinesOffered,
+                specialDishOne: truck.threeSpecialDishes[0].dish,
+                specialDishTwo: truck.threeSpecialDishes[1].dish,
+                specialDishThree: truck.threeSpecialDishes[2].dish,
+                famousFor: truck.famousFor,
+                description: truck.description,
+                year: truck.servingSince,
+                discountToday: truck.discountToday,
+                customDiscount1: truck.customDiscount1,
+                customDiscount2: truck.customDiscount2,
+                customDiscount3: truck.customDiscount3,
+                email: truck.email,
+                contactNumbers: truck.contactNumbers,
+                bogoOn: truck.bogoOn,
+                website: truck.website
+            })
         })
         .catch(err => {
             console.log(err);
-            console.log('Truck could not be found for editing');
+            console.log('Truck not found');
             return res.redirect('/myTrucks')
         })
+
+}
+exports.postEditMyTruck = (req, res, next) => {
+    const truckId = req.body.truckId;
+    const errors = validationResult(req);
+    1
+    if (!errors.isEmpty()) {
+        let className = 'errorFlash'
+        return res.render('dashboard/addFoodTruck', {
+            message: errors.array()[0].msg,
+            className: className,
+            pageTitle: 'Edit Food Truck',
+            url: '/editFoodTruck',
+            truckId: truckId,
+            name: req.body.name,
+            address: req.body.address,
+            priceForTwo: req.body.priceForTwo,
+            mondayStatus: req.body.mondayStatus,
+            tuesdayStatus: req.body.tuesdayStatus,
+            wednesdayStatus: req.body.wednesdayStatus,
+            thursdayStatus: req.body.thursdayStatus,
+            fridayStatus: req.body.fridayStatus,
+            saturdayStatus: req.body.saturdayStatus,
+            sundayStatus: req.body.saturdayStatus,
+            openingTimeMonday: req.body.openingTimeMonday,
+            closingTimeMonday: req.body.closingTimeMonday,
+            openingTimeTuesday: req.body.openingTimeTuesday,
+            closingTimeTuesday: req.body.closingTimeTuesday,
+            openingTimeWednesday: req.body.openingTimeWednesday,
+            closingTimeWednesday: req.body.closingTimeWednesday,
+            openingTimeThursday: req.body.openingTimeThursday,
+            closingTimeThursday: req.body.closingTimeThursday,
+            openingTimeFriday: req.body.openingTimeFriday,
+            closingTimeFriday: req.body.closingTimeFriday,
+            openingTimeSaturday: req.body.openingTimeSaturday,
+            closingTimeSaturday: req.body.closingTimeSaturday,
+            openingTimeSunday: req.body.openingTimeSunday,
+            closingTimeSunday: req.body.closingTimeSunday,
+            cusinesOffered: req.body.cusinesOffered,
+            specialDishOne: req.body.specialDishOne,
+            specialDishTwo: req.body.specialDishTwo,
+            specialDishThree: req.body.specialDishThree,
+            famousFor: req.body.famousFor,
+            description: req.body.description,
+            year: req.body.year,
+            discountToday: req.body.discountToday,
+            customDiscount1: req.body.customDiscount1,
+            customDiscount2: req.body.customDiscount2,
+            customDiscount3: req.body.customDiscount3,
+            email: req.body.email,
+            contactNumbers: req.body.contactNumbers,
+            bogoOn: req.body.bogoOn,
+            website: req.body.website
+        })
+    } else {
+        FoodTruck.findById(truckId)
+            .then(truck => {
+                truck.name = req.body.name;
+                truck.address = req.body.address;
+                truck.priceForTwo = req.body.priceForTwo;
+
+                truck.openingHours[0].open = req.body.MondayStatus;
+                truck.openingHours[1].open = req.body.TuesdayStatus;
+                truck.openingHours[2].open = req.body.WednesdayStatus;
+                truck.openingHours[3].open = req.body.ThursdayStatus;
+                truck.openingHours[4].open = req.body.FridayStatus;
+                truck.openingHours[5].open = req.body.SaturdayStatus;
+                truck.openingHours[6].open = req.body.SundayStatus;
+
+                truck.openingHours[0].openingTime = req.body.openingTimeMonday;
+                truck.openingHours[0].closingTime = req.body.closingTimeMonday;
+
+                truck.openingHours[1].openingTime = req.body.openingTimeTuesday;
+                truck.openingHours[1].closingTime = req.body.closingTimeTuesday;
+
+                truck.openingHours[2].openingTime = req.body.openingTimeWednesday;
+                truck.openingHours[2].closingTime = req.body.closingTimeWednesday;
+
+                truck.openingHours[3].openingTime = req.body.openingTimeThursday;
+                truck.openingHours[3].closingTime = req.body.closingTimeThursday;
+
+                truck.openingHours[4].openingTime = req.body.openingTimeFriday;
+                truck.openingHours[4].closingTime = req.body.closingTimeFriday;
+
+                truck.openingHours[5].openingTime = req.body.openingTimeSaturday;
+                truck.openingHours[5].closingTime = req.body.closingTimeSaturday;
+
+                truck.openingHours[6].openingTime = req.body.openingTimeSunday;
+                truck.openingHours[6].closingTime = req.body.closingTimeSunday;
+
+                truck.cusinesOffered = req.body.cusinesOffered;
+
+                truck.specialDishOne = req.body.specialDishOne;
+                truck.specialDishTwo = req.body.specialDishTwo;
+                truck.specialDishThree = req.body.specialDishThree;
+
+                truck.famousFor = req.body.famousFor;
+                truck.description = req.body.description;
+                truck.year = req.body.year;
+                truck.discountToday = req.body.discountToday;
+
+                truck.customDiscount1 = req.body.customDiscount1;
+                truck.customDiscount2 = req.body.customDiscount2;
+                truck.customDiscount3 = req.body.customDiscount3;
+
+                truck.email = req.body.email;
+                truck.contactNumbers = req.body.contactNumbers;
+
+                truck.bogoOn = req.body.bogoOn;
+                truck.website = req.body.website;
+
+
+                truck.save()
+                    .then(result => {
+                        console.log(result);
+                        return res.redirect('/myTrucks');
+                    }).catch(err => {
+                        // console.log(err);
+                        console.log('Truck found but could not be updated');
+                        return res.redirect('/myTrucks');
+                    })
+            })
+            .catch(err => {
+                // console.log(err);
+                console.log('Truck could not be found for editing');
+                return res.redirect('/myTrucks')
+            })
+    }
 }
--- Routes/dashboard.js
+++ Routes/dashboard.js
@@ -18,8 +18,67 @@ router.get('/myTrucks', isAuth, dashboardController.getMyTrucks)
 
 // Edit Truck
 router.get('/editFoodTruck/:truckId', isAuth, dashboardController.getEditMyTruck)
+router.post('/postEditFoodTruck', [
+    body('name').custom((value => {
+        if (value === '') {
+            throw new Error('Please enter the name of your food truck')
+        }
+        return true;
+    })),
+    body('email').isEmail().withMessage('Please Enter A Valid Email'),
+    body('contactNumber').custom((value => {
+        if (value === '') {
+            throw new Error('Please enter the contact number of your food truck')
+        }
+        return true;
+    })),
+    body('priceForTwo').custom((value => {
+        if (value === '') {
+            throw new Error('Please enter price for two')
+        }
+     
+        return true;
+    })),
+    body('specialDishOne').custom((value => {
+        if (value === '') {
+            throw new Error('Please fill the special dish one field')
+        }
+        return true;
+    })),
+    body('specialDishTwo').custom((value => {
+        if (value === '') {
+            throw new Error('Please fill the special dish two field')
+        }
+        return true;
+    })),
+    body('specialDishThree').custom((value => {
+        if (value === '') {
+            throw new Error('Please fill the special dish three field')
+        }
+        return true;
+    })),
+    body('cusinesOffered').custom((value => {
+        if (value === '') {
+            throw new Error('Please enter the cusines offered')
+        }
+        return true;
+    })),
+    body('famousFor').custom((value => {
+        if (value === '') {
+            throw new Error('Please fill out the famous for field')
+        }
+        return true;
+    })),
+    body('description').custom((value => {
+        if (value === '') {
+            throw new Error('Please enter description about your food truck')
+        }
+        return true;
+    }))
+], isAuth, dashboardController.postEditMyTruck)
 // Post Food Truck
-router.post('/addFoodTruck', [
+router.post('/addFoodTruck',
+ [
     body('name').custom((value => {
         if (value === '') {
             throw new Error('Please enter the name of your food truck')
